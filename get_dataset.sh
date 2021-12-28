22379dbe49a01e8f91ea926842f82b39ca7e5e7d
ntudlcv <72860306+ntudlcv@users.noreply.github.com>
2021-12-28 11:51:56 +0800

    Update get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,5 +1,5 @@
 # Download dataset
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1XY5twJuNLU-tJL-sr5-efTMPqbOtodS6' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1XY5twJuNLU-tJL-sr5-efTMPqbOtodS6" -O food_data.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1IYWPK8h9FWyo0p4-SCAatLGy0l5omQaw' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1IYWPK8h9FWyo0p4-SCAatLGy0l5omQaw" -O food_data.zip && rm -rf /tmp/cookies.txt
 
 # Unzip the downloaded zip file
 unzip ./food_data.zip
